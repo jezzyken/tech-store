bc5973a8d89e051c532f299d1f50a590ab640bad
jezzyken <jezrelljolmpong17@gmail.com>
2020-07-24 12:17:03 +0800

    setting up context api

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,13 @@
 import React from "react";
 
+import { ProductConsumer } from "../context";
+
 export default function HomePage() {
-  return <div>home page</div>;
+  return (
+    <ProductConsumer>
+      {(value) => {
+        return <div>home page {value}</div>;
+      }}
+    </ProductConsumer>
+  );
 }
